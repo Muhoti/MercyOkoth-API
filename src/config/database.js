afeed52cdd49b806181cd9ad1184d10410f7dbd6
muhoti <strongmuhoti@gmail.com>
2025-01-02 22:56:28 +0300

    database

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
-);
+const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: process.env.PGDIALECT,
+    logging: false,
+    dialectOptions: {
+        ssl: false // set to true if your database requires SSL
+    }
+});
 
 module.exports = sequelize;
