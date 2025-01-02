e8d12f998bacf66bf5eabcea9c1627b975ead1e0
muhoti <strongmuhoti@gmail.com>
2025-01-02 18:24:34 +0300

    db

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`
+    `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
 );
 
-module.exports = sequelize; 
+module.exports = sequelize;
